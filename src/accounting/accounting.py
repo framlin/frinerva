c3651b8b6429f287c4c98cc59e96f82255dbf4b8
framlin <w.egger@framlin.com>
2021-02-07 15:00:04 +0100

    - make Accounting more pythonic, by making it iterable

--- src/accounting/accounting.py
+++ src/accounting/accounting.py
@@ -12,11 +12,33 @@ class Accounting:
     def get_balance(self, booking_period: int) -> Balance:
         return self._balances[str(booking_period)]
 
+    def get_balances(self) -> dict:
+        return self._balances
+
     def load(self, path):
         subfolders = [f.path for f in os.scandir(path) if f.is_dir()]
         for booking_period_path in subfolders:
             booking_period = os.path.basename(booking_period_path)
             if booking_period.isdigit():
                 self.add_balance(Balance(int(booking_period)))
-        for balance in self._balances.values():
+
+        for balance in self:
             balance.load(path)
+
+    def __iter__(self):
+        return AccountingIterator(self)
+
+
+class AccountingIterator:
+    def __init__(self, accounting):
+        self._balances = accounting.get_balances()
+        self._keys = list(self._balances.keys())
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._keys):
+            result = self._balances[self._keys[self._index]]
+            self._index += 1
+            return result
+
+        raise StopIteration
